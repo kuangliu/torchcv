b210ff3f99fae3fe6bb5fb60d1e00573ce7ff667
kuangliu <kuang.liu@hotmail.com>
2017-12-26 15:07:32 +0800

    Add SSD to model init file

--- torchcv/models/__init__.py
+++ torchcv/models/__init__.py
@@ -2,5 +2,8 @@ from torchcv.models.retinanet.fpn import FPN50
 from torchcv.models.retinanet.box_coder import BoxCoder
 from torchcv.models.retinanet.retinanet import RetinaNet
 
+from torchcv.models.ssd.net import SSD300
+from torchcv.models.ssd.box_coder import SSDBoxCoder
+
 from torchcv.models.loss.ssd_loss import SSDLoss
 from torchcv.models.loss.focal_loss import FocalLoss
