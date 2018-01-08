802da47ee9f1002a7fa125c93208bda60c80f85c
kuangliu <kuang.liu@hotmail.com>
2018-01-08 17:19:01 +0800

    Add ssd demo

---
+++ examples/ssd/demo.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from PIL import Image, ImageDraw
+from torch.autograd import Variable
+from torchcv.models.ssd import SSD300, SSDBoxCoder
+
+
+print('Loading model..')
+net = SSD300(num_classes=21)
+net.load_state_dict(torch.load('./examples/ssd/checkpoint/params.pth'))
+net.eval()
+
+print('Loading image..')
+img = Image.open('./image/000020.jpg')
+ow = oh = 300
+img = img.resize((ow,oh))
+
+print('Predicting..')
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
+])
+x = transform(img)
+x = Variable(x, volatile=True)
+loc_preds, cls_preds = net(x.unsqueeze(0))
+
+print('Decoding..')
+box_coder = SSDBoxCoder()
+boxes, labels, scores = box_coder.decode(
+    loc_preds.data.squeeze(), F.softmax(cls_preds.squeeze(), dim=1).data)
+print(labels)
+print(scores)
+
+draw = ImageDraw.Draw(img)
+for box in boxes:
+    draw.rectangle(list(box), outline='red')
+img.show()
